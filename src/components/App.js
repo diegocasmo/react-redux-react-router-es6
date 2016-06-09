9088bc16607b08e6943d03e7f81b292016a2ec8a
Diego Castillo <diegocasmo@gmail.com>
2016-06-08 19:52:02 -0500

    Initial App Structure

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+// This component handles the app
+// template which other pages use
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header/>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
